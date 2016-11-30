afb4250a90c9be8638dde7832ee58bfceabc2b23
Vieira-zj <zhengjin@fun.tv>
2016-11-30 15:05:51 +0800

    Do refactor for run instrument tests script.

--- ZJPyTools/RunAndroidInstrumentTests.py
+++ ZJPyTools/RunAndroidInstrumentTests.py
@@ -9,12 +9,12 @@ import sys
 import time
 import logging
 
-from ZJPyUtils import WinSysUtils,AdbUtils,LogUtils,FileUtils
+from ZJPyUtils import WinSysUtils, AdbUtils, LogUtils, FileUtils
 
 # ----------------------------------------------------
-# Variables
+# Global vars
 # ----------------------------------------------------
-g_device_ip = '172.17.5.106'
+g_device_ip = '172.17.5.133'
 
 g_test_class = 'com.example.zhengjin.funsettingsuitest.testsuites.Launcher24x7TestsSuite'
 g_component = 'com.example.zhengjin.funsettingsuitest.test'
@@ -23,151 +23,156 @@ g_test_runner = 'android.support.test.runner.AndroidJUnitRunner'
 g_total_run_times = 0
 g_total_failed = 0
 
-g_tmp_dir_path = '/data/local/tmp'
-g_tmp_captures_dir_path = '%s/captures' %g_tmp_dir_path
+g_remote_tmp_dir_path = '/data/local/tmp'
+g_remote_tmp_captures_dir_name = 'AutoTestCaptures'
+g_remote_tmp_captures_dir_path = '%s/%s' % (g_remote_tmp_dir_path, g_remote_tmp_captures_dir_name)
 
-g_report_dir_path = ''
-g_report_file_path = ''
-g_log_file_path = ''
 
-def init_report_paths():
-    report_dir_name = 'logs_%s' %WinSysUtils.get_current_date_and_time()
+# ----------------------------------------------------
+# Init local path
+# ----------------------------------------------------
+g_local_report_dir_path = ''
+g_local_run_log_file_path = ''
+g_local_logging_file_path = ''
+
+def init_local_report_paths():
+    report_dir_name = 'logs_%s' % WinSysUtils.get_current_date_and_time()
     report_dir_path = os.path.join(os.getcwd(), 'logs', report_dir_name)
+    global g_local_report_dir_path
+    g_local_report_dir_path = report_dir_path
+
     if not os.path.exists(report_dir_path):
         os.makedirs(report_dir_path)
-    global g_report_dir_path
-    g_report_dir_path = report_dir_path
+
+    global g_local_run_log_file_path
+    run_log_file_name = 'InstrutmentRunLog.log'
+    g_local_run_log_file_path = os.path.join(report_dir_path, run_log_file_name)
     
-    report_name = 'InstrutmentTestReport.log'
-    report_file_path = os.path.join(report_dir_path, report_name)
-    global g_report_file_path
-    g_report_file_path = report_file_path
-    
-    log_name = 'InstrumentRunnerLog.log'
-    log_file_path = os.path.join(report_dir_path, log_name)
-    global g_log_file_path
-    g_log_file_path = log_file_path
+    global g_local_logging_file_path
+    logging_name = 'InstrutmentLoggingReport.log'
+    g_local_logging_file_path = os.path.join(report_dir_path, logging_name)
 
 
 # ----------------------------------------------------
 # Helper functions
 # ----------------------------------------------------
-def rm_old_captures_on_shell():
-    cmd = 'adb shell ls %s' %g_tmp_dir_path
-    output_lines = WinSysUtils.run_sys_cmd_and_ret_lines(cmd)
+def rm_old_captures_on_remote():
+    cmd = 'adb shell ls %s' % g_remote_tmp_dir_path
+    lines_files = WinSysUtils.run_sys_cmd_and_ret_lines(cmd)
     flag_found = False
-    for line in output_lines:
-        if 'captures' in line:
+    for line in lines_files:
+        if g_remote_tmp_captures_dir_name in line:
             flag_found = True
+            break
     
-    if flag_found:  # if dir exist, rm old files
-        cmd = 'adb shell rm %s/*.png' %g_tmp_captures_dir_path
+    if flag_found:
+        cmd = 'adb shell ls %s' % g_remote_tmp_captures_dir_path
+        lines_capture = WinSysUtils.run_sys_cmd_and_ret_lines(cmd)
+        if len(lines_capture) > 0:
+            cmd = 'adb shell rm %s/*.png' % g_remote_tmp_captures_dir_path
+            WinSysUtils.run_sys_cmd(cmd)
+    else:
+        cmd = 'adb shell mkdir -p %s' % g_remote_tmp_captures_dir_path
         WinSysUtils.run_sys_cmd(cmd)
-    else:  # if not, create dir
-        cmd = 'adb shell mkdir -p %s' %g_tmp_captures_dir_path
-        WinSysUtils.run_sys_cmd(cmd)
 
-def copy_and_pull_captures():
-    captures_dir_path = '/sdcard/auto_test_captures/captures_%s' %WinSysUtils.get_current_date_and_time()
-
-    cmd = 'adb shell mkdir -p %s' %captures_dir_path  # make dir for multiple levels
-    WinSysUtils.run_sys_cmd(cmd)
-    time.sleep(1)
-    
-    cmd = 'adb shell busybox cp %s/*.png %s' %(g_tmp_captures_dir_path, captures_dir_path)
-    WinSysUtils.run_sys_cmd(cmd)
-    time.sleep(1)
-
-    win_captures_dir_path = '%s/captures' %g_report_dir_path
-    if not os.path.exists(win_captures_dir_path):
-        os.mkdir(win_captures_dir_path)
-    cmd = 'adb pull %s %s' %(captures_dir_path, win_captures_dir_path)
+def pull_remote_captures_to_local():
+    # pull both dir and files in the dir from remote
+    cmd = 'adb pull %s %s' % (g_remote_tmp_captures_dir_path, g_local_report_dir_path)
     WinSysUtils.run_sys_cmd(cmd)
 
 def build_instrument_cmd_v1():
     return 'adb shell am instrument -w -r -e debug false -e class %s %s/%s >> %s' \
-        %(g_test_class,g_component,g_test_runner,g_report_file_path)
+        % (g_test_class, g_component, g_test_runner, g_local_run_log_file_path)
 
 def build_instrument_cmd_v2():
     return 'adb shell am instrument -w -r -e debug false -e class %s %s/%s' \
-        %(g_test_class,g_component,g_test_runner)
+        % (g_test_class, g_component, g_test_runner)
 
 def run_instrument_tests_v1(cmd):
     WinSysUtils.run_sys_cmd(cmd)
 
 def run_instrument_tests_v2(cmd):
-    output_content = WinSysUtils.run_sys_cmd_and_ret_content(cmd)
-    if output_content is None:
+    input_lines = WinSysUtils.run_sys_cmd_and_ret_lines(cmd)
+    if len(input_lines) == 0:
         return
-    FileUtils.append_content_to_file(g_report_file_path, output_content)
     
-    check_test_case_force_closed(output_content)
-    check_test_case_failed(output_content)
+    output_lines = []
+    for line in input_lines:
+        output_lines.append(line.rstrip('\r\n')  + '\n')
+    FileUtils.append_lines_to_file(g_local_run_log_file_path, (output_lines))
     
-def check_test_case_failed(content):
+    check_test_case_force_closed(output_lines)
+    check_test_case_failed(output_lines)
+    
+def check_test_case_failed(lines):
     global g_total_failed
-    if 'AssertionFailedError' in content:
-        g_total_failed += 1
+    for line in lines:
+        if 'AssertionFailedError' in line:
+            g_total_failed += 1
 
-def check_test_case_force_closed(content):
+def check_test_case_force_closed(lines):
     '''
-    force close the testing process by Java code System.exit(0), 
+    force close the testing process by Java code System.exit(1), 
     then check the keyword "Process crashed" in the testing runner, 
     if yes, stop the testing runner at once.
     '''
-    if 'Process crashed' in content:
-        logging.error('Found issue(Buffer Refresh Failed), and testing runner process is force closed.')
-        sys.exit(1)
+    for line in lines:
+        if 'Process crashed' in line:
+            logging.error('Found issue(Buffer Refresh Failed), and testing runner process is force closed.')
+            sys.exit(1)
 
 def create_report_summary():
     logging.info('----- Testing report summary: ')
-    logging.info('Total run times: %d' %g_total_run_times)
-    logging.info('Total passed: %d' %(g_total_run_times - g_total_failed))
-    logging.info('Total failed: %d' %g_total_failed)
+    logging.info('Total run times: %d' % g_total_run_times)
+    logging.info('Total passed: %d' % (g_total_run_times - g_total_failed))
+    logging.info('Total failed: %d' % g_total_failed)
 
 
 # ----------------------------------------------------
 # Main
 # ----------------------------------------------------
 def loop_run_test(during):
-    global g_total_run_times
-
-    i = 1
+    i = 0
     cmd = build_instrument_cmd_v2()
-
     start_time = int(time.clock())
+    
     while 1:
-        logging.info('run test %d times.' %i)
         if not AdbUtils.verify_adb_devices_serialno():
             if not AdbUtils.adb_connect_to_device(g_device_ip):
+                logging.error('device is disconnect!')
                 exit(1)
         
         run_instrument_tests_v2(cmd)
-        i += 1
         time.sleep(3)
         
+        i += 1
+        logging.info('run ui test %d times.' % i)
         cur_run_time = int(time.clock()) - start_time
-        logging.info('current run time: %d minutes' %(cur_run_time/60))
+        logging.info('current run time: %d minutes %d seconds' % (cur_run_time / 60, cur_run_time % 60))
+        
         if ((cur_run_time - start_time) > during):
             break
     # END LOOP
+
+    global g_total_run_times
     g_total_run_times = i
 
 def run_test_setup(during):
-    init_report_paths()
-    LogUtils.init_log_config(logging.DEBUG, logging.INFO, g_log_file_path)
+    init_local_report_paths()
+    LogUtils.init_log_config(logging.DEBUG, logging.INFO, g_local_logging_file_path)
 
     if not AdbUtils.adb_connect_with_root(g_device_ip):
+        logging.error('adb is NOT run with root!')
         exit(1)
-    rm_old_captures_on_shell()
+    rm_old_captures_on_remote()
 
     test_case = g_test_class[(g_test_class.rindex('.') + 1) : ]
-    logging.info('----- START run test %s for %s minutes' %(test_case, during/60))
+    logging.info('----- START run test %s for %s minutes' % (test_case, during / 60))
 
 def run_test_clearup():
     create_report_summary()
     logging.info('----- END run test')
-    copy_and_pull_captures()
+    pull_remote_captures_to_local()
 
 def main(during):
     run_test_setup(during)
@@ -177,9 +182,8 @@ def main(during):
 
 if __name__ == '__main__':
 
-    run_during = 13 * 60 * 60   # seconds
-    main(run_during)
+    run_time = 5 * 60  # seconds
+    main(run_time)
 
-    
-    print '%s done!' %(os.path.basename(__file__))
+    print '%s done!' % (os.path.basename(__file__))
     pass
